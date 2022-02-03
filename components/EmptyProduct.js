a919db140cb9a75dfe96db2c5b49bf8e672399cc
Mukarram Ali <mukarram.ali89@gmail.com>
2022-02-03 22:07:51 +0100

    feat: allow to choose product all the time

--- components/EmptyProduct.js
+++ components/EmptyProduct.js
@@ -1,38 +1,17 @@
-import { ResourcePicker } from "@shopify/app-bridge-react";
 import { EmptyState } from "@shopify/polaris";
-import { useCallback, useState } from "react";
 const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
 
-export function EmptyProduct({ setProduct }) {
-  const [open, setOpen] = useState(false);
-  const handleSelection = useCallback(
-    (resources) => {
-      setOpen(false);
-      setProduct(resources.selection[0]);
-    },
-    [setProduct]
-  );
-
+export function EmptyProduct({ setOpen }) {
   return (
-    <>
-      <ResourcePicker
-        resourceType="Product"
-        showVariants={false}
-        open={open}
-        onSelection={(resources) => handleSelection(resources)}
-        onCancel={() => setOpen(false)}
-        allowMultiple={false}
-      />
-      <EmptyState
-        heading="Canonical Tag"
-        action={{
-          content: "Select Product",
-          onAction: () => setOpen(true),
-        }}
-        image={img}
-      >
-        <p>Select products to change their canonical tag.</p>
-      </EmptyState>
-    </>
+    <EmptyState
+      heading="Canonical Tag"
+      image={img}
+      action={{
+        content: "Choose Product",
+        onAction: setOpen,
+      }}
+    >
+      <p>Select products to change their canonical tag.</p>
+    </EmptyState>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,41 @@
-import { Layout, Page } from "@shopify/polaris";
-import { useState } from "react";
+import { ResourcePicker } from "@shopify/app-bridge-react";
+import { Button, Layout, Page } from "@shopify/polaris";
+import { useCallback, useState } from "react";
 import { EmptyProduct } from "../components/EmptyProduct";
 import { ProductForm } from "../components/ProductForm";
 
 const Index = () => {
   const [product, setProduct] = useState();
+  const [open, setOpen] = useState(false);
+  const handleSelection = useCallback(
+    (resources) => {
+      setOpen(false);
+      setProduct(resources.selection[0]);
+    },
+    [setProduct]
+  );
+
   return (
     <Page>
       <Layout>
+        <Layout.Section>
+          <Button primary={true} onClick={() => setOpen(true)}>
+            Choose Product
+          </Button>
+        </Layout.Section>
         {product ? (
           <ProductForm product={product} />
         ) : (
-          <EmptyProduct setProduct={setProduct} />
+          <EmptyProduct setOpen={setOpen} />
         )}
+        <ResourcePicker
+          resourceType="Product"
+          showVariants={false}
+          open={open}
+          onSelection={(resources) => handleSelection(resources)}
+          onCancel={() => setOpen(false)}
+          allowMultiple={false}
+        />
       </Layout>
     </Page>
   );
