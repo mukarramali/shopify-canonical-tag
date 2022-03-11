eda4a707c57ff973db1b9bce7311bcae1272f737
Mukarram Ali <mukarram.ali89@gmail.com>
2022-03-11 20:18:34 +0100

    feat: persist language selection

--- app/providers/LanguageProvider.js
+++ app/providers/LanguageProvider.js
@@ -1,13 +1,22 @@
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export const LanguageContext = React.createContext("en");
+const LocalStorageKey = "canonical-tag-app-language";
 
 export const LanguageProvider = ({ children }) => {
   const [locale, setLocale] = React.useState("en");
 
+  useEffect(() => {
+    const savedLanguage = localStorage.getItem(LocalStorageKey);
+    if (savedLanguage) {
+      setLocale(savedLanguage);
+    }
+  }, []);
+
   const changeLanguage = useCallback(
     (value) => {
       setLocale(value);
+      localStorage.setItem(LocalStorageKey, value);
     },
     [setLocale]
   );
