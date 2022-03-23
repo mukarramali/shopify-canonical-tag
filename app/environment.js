06dedab0adaa0a26efbef9ce1e69048287eaa885
Mukarram Ali <mukarram.ali89@gmail.com>
2022-03-23 19:21:56 +0100

    feat: add redis to persist data

--- app/environment.js
+++ app/environment.js
@@ -1,1 +1,3 @@
 export const EXTENSION_UUID = process.env.NEXT_PUBLIC_EXTENSION_UUID;
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+export const REDIS_SERVER = process.env.REDIS_SERVER;
